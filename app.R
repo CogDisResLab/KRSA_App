a199a11e36bb0579c18f0ec3011b4a31ad2cba17
kalganem <khaled.a.alganem@gmail.com>
2020-09-11 12:47:32 -0400

    Final for subm

--- app.R
+++ app.R
@@ -33,7 +33,6 @@ ui <- shinyUI(fluidPage(
             
             fileInput('file0', label = h3("Upload raw data file"),
                       accept=c('text/csv', 'text/comma-separated-values','text/plain', '.csv')),
-            actionButton("upload1", "upload"),
             #Options for file type including header, seperator, and quotes.
             h4("File options:"),
             checkboxInput('header1', 'Header', TRUE),
@@ -48,16 +47,7 @@ ui <- shinyUI(fluidPage(
                            'Single Quote'="'"),
                          '"'),
             tags$hr(),
-            #TODO: uncomment below for PINET
-            # radioButtons("radio", label = h3("Chose your kinase-peptide association option"),
-            #              choices = list("PiNET" = 1, "Upload" = 2), 
-            #              selected = 2),
-            
-            #This takes a tab or comma seperated file as input, with the first column "Substrates" and
-            #the second column "Kinases" being the only two columns and both required.
             conditionalPanel(
-                #TODO: uncomment below for PINET and comment out "condition = TRUE"
-                #condition = "input.radio==2",
                 condition = TRUE,
                 fileInput('file1', label = h5("Kinase-peptide association file"),
                           accept=c('text/csv', 'text/comma-separated-values','text/plain', '.csv')),
@@ -120,16 +110,10 @@ ui <- shinyUI(fluidPage(
                          fluidRow(
                              h3("Preview of all data"),
                              DT::dataTableOutput(outputId="contents0"),
-                             #DT::dataTableOutput('contents0'),
-                             #column(6,
-                             #DT::dataTableOutput('contents1'),
                              h3("Preview of control data"),
                              DT::dataTableOutput(outputId="contents1"),
-                             #),
-                             #column(6,
                              h3("Preview of experimental data"),
                              DT::dataTableOutput(outputId="contents2")
-                             #)
                          )
                          
                 ),
@@ -153,15 +137,12 @@ ui <- shinyUI(fluidPage(
                          downloadButton('downloadDataFC2', 'Download Substrate Results 2'),
                          selectInput("pep_sel", "Choose a Substrate:", choices=c()), 
                          plotOutput("pep_plot", width = "100%", height="600px"),
-                         downloadButton('downloadData_LR', 'Download Graph'),
+                         downloadButton('download_pep_plot', 'Download Graph'),
                          hr()
-                         #dataTableOutput("subTable")
-                         
+
                 ),
                 tabPanel("Step 3: Iteration", 
-                         #This is the slider that allows you to choose the number of iterations of sampling you
-                         #want to do, from 0 to 5000 (1000 default) and jumping in increments of 100.
-                         sliderInput("itr_num", label = h4("Select the number of iterations"), min = 0, 
+                         sliderInput("itr_num", label = h4("Select the number of iterations"), min = 100, 
                                      max = 5000, value = 1000, step=100, width='100%'),
                          tags$hr(),
                          h4("Confirm and Run"),
@@ -174,12 +155,12 @@ ui <- shinyUI(fluidPage(
                 ),
                 tabPanel("Step 4: Results",
                          DT::dataTableOutput("overallTable"),
-                         downloadButton('downloadDataR', 'Download Results')
+                         downloadButton('download_krsa_table', 'Download Results')
                 ),
                 tabPanel("Step 5: Histograms",
                          selectInput("kin_sel", "Choose a Kinase:", choices=c()), 
                          plotOutput("kin_plot", width = "100%", height="600px"),
-                         downloadButton('downloadData', 'Download Histogram')
+                         downloadButton('download_hist', 'Download Histogram')
                 ),
                 tabPanel("Step 6: Heatmap",
                          fluidRow(
@@ -193,7 +174,7 @@ ui <- shinyUI(fluidPage(
                                     selectInput("low", "Choose low phosphorylation color:", choices=c("cyan", "orange", "yellow", "green", "darkgreen", "red", "blue", "purple", "magenta")),
                                     selectInput("mid", "Choose middle phosphorylation color:", choices=c("black", "white")),
                                     selectInput("high", "Choose high phosphorylation color:", choices=c("yellow", "orange", "red", "green", "darkgreen", "cyan", "blue", "purple", "magenta")),
-                                    downloadButton('downloadDataH', 'Download Heatmap')
+                                    downloadButton('download_heatmap', 'Download Heatmap')
                              )
                          ),
                          
@@ -203,17 +184,14 @@ ui <- shinyUI(fluidPage(
                 tabPanel("Step 7: Network",
                          fluidRow(
                              column(6,
-                                    #sliderInput("sliderExpans", label = "Generations", min = 1, max = 3, value=1),
                                     sliderInput("nodeSize", label = "Node Size", min=1, max=10, value=3, step=1),
                                     sliderInput("nodeTextSize", label = "Node Text Size", min=1, max=10, value=6, step=1)
-                                    #checkboxInput("low_conf", "Include Low Confidence Connections", FALSE),
-                                    #selectInput("interType", "Interaction Type", choices=c("All Interactions", "Kinase Only"))
                              ),
                              column(6,
                                     selectInput("hitsColor", "Choose color for KRSA kinases:", choices=c("red","cyan", "orange", "yellow", "green", "darkgreen", "blue", "purple", "magenta", "white", "gray")),
                                     selectInput("interColor", "Choose color for interacting proteins:", choices=c("gray","red", "orange", "yellow", "green", "darkgreen", "cyan", "blue", "purple", "magenta", "white")),
                                     selectInput("layout", "Choose layout:", choices=c("Circle", "Fruchterman-Reingold", "Kamada Kawai", "LGL")),
-                                    downloadButton('downloadDataN', 'Download Network')
+                                    downloadButton('download_net', 'Download Network')
                              )
                          ),
                          hr(),
@@ -240,49 +218,45 @@ server <- function(input, output, session) {
       kin_hits = "",
       ctl_samples = "",
       case_sample = "",
-      barcodes = ""
+      barcodes = "",
+      sample_ids = ""
     )
     
-    
-    
-    #Reactive dataset for the input file 0 (raw data).
+    plots <- reactiveValues()
+ 
     data_set0 <- reactive({
         req(input$file0)
         inFile <- input$file0
         x<-read.csv(inFile$datapath, header=F, 
                             sep=input$sep1, quote=input$quote1, stringsAsFactors = F)
-        reading_data(x)
-         
+        tidy_data <- reading_data(x)
+        tidy_data %>% pull(Barcode) %>% unique() %>% as.list() -> barcode_ids
+        shared_values$barcodes <- barcode_ids
+        
+        tidy_data %>% pull(`Sample name`) %>% unique() %>% as.list() -> samples_ids
+        shared_values$sample_ids <- samples_ids
+        
+        tidy_data
 
     })
     
+    observe({
+      updateCheckboxGroupInput(session, "celltype",
+                               choices = shared_values$barcodes,
+                               selected = shared_values$barcodes)
+    })
     
-      observeEvent(input$upload1,{
-        #z <- input$file0
-        #data_set0() ->> xy
-        data_set0() %>% pull(Barcode) %>% unique() %>% as.list() -> barcode_ids
-        #cont <<- pull(data_set0(),Barcode) %>% unique()
-        updateCheckboxGroupInput(session, "celltype",
-                                 choices = barcode_ids
-                                 #selected = NA #This change made it so all of the boxes were selected by default
-        )
-        
-        
-        data_set0() %>% pull(`Sample name`) %>% unique() %>% as.list() -> samples_ids
-        
-        updateCheckboxGroupInput(session, "controlSamples",
-                                 choices = samples_ids
-                                 #selected = NA #This change made it so all of the boxes were selected by default
-        )
-        
-        updateCheckboxGroupInput(session, "experimentalSamples",
-                                 choices = samples_ids
-                                 #selected = NA #This change made it so all of the boxes were selected by default
-        )
-        
-      })
+    observe({  
+      updateCheckboxGroupInput(session, "controlSamples",
+                               choices = shared_values$sample_ids)
+    })
       
-      
+    observe({
+      updateCheckboxGroupInput(session, "experimentalSamples",
+                               choices = shared_values$sample_ids)
+
+    })
+ 
       observeEvent(input$calculateData, {
         
         data_set0() %>% filter(`Sample name` %in% c(input$controlSamples, input$experimentalSamples)) -> filtered_samples
@@ -290,11 +264,12 @@ server <- function(input, output, session) {
         filtered_samples %>% filter(`Exposure time` == 200) %>%
           select(Peptide, `Sample name`, Signal) %>% 
           spread(key = `Sample name`, value = Signal) %>% 
-          dplyr::filter_at( vars(-Peptide) , all_vars(. >= as.numeric(2))) %>% 
-          dplyr::filter(!Peptide %in% c("pVASP_150_164", "pTY3H_64_78", "ART_025_CXGLRRWSLGGLRRWSL")) %>%
-          pull(Peptide) -> ppPassAll
+          dplyr::filter(!Peptide %in% c("pVASP_150_164", "pTY3H_64_78", "ART_025_CXGLRRWSLGGLRRWSL")) -> PassDF
         
+        pull(PassDF, Peptide) -> ppPassAll
 
+        dplyr::filter_at(PassDF,  vars(-Peptide) , all_vars(. >= as.numeric(2))) %>% 
+          pull(Peptide) -> ppPass_min
         
         filtered_samples %>% dplyr::filter(Peptide %in% ppPassAll, Cycle == 124) %>% 
           select(Peptide, `Sample name`, `Exposure time`, Signal) %>% 
@@ -310,12 +285,6 @@ server <- function(input, output, session) {
           ) %>% 
           select(`Sample name`, Peptide, slope, r.seq) -> Dataset_PW_C1_Fit
         
-        # Dataset_PW_C1_Fit %>% 
-        #   select(`Sample name`, Peptide, r.seq) %>% spread(`Sample name`, r.seq) %>% 
-        #   filter_at( vars(-Peptide) , all_vars(. >= 0.9)) %>% pull(Peptide) -> ppPassR2
-        
-        #pass_both <- intersect(ppPassAll, ppPassR2)
-        
         Dataset_PW_C1_Fit %>% filter(Peptide %in% ppPassAll) %>% 
           mutate(Group = ifelse(`Sample name` %in% input$controlSamples, "Control", "Case")) %>% 
           group_by(Group, Peptide) %>% 
@@ -325,20 +294,32 @@ server <- function(input, output, session) {
           select(Group, Peptide, r.seqAvg) %>% spread(Group, r.seqAvg) %>% 
           filter_at( vars(-Peptide) , all_vars(. >= 0.9)) %>% pull(Peptide) -> ppPassR2_avg
         
-        #setdiff(ppPassR2_avg, ppPassR2)
+        pass_both_2 <- intersect(ppPass_min, ppPassR2_avg)
         
-        pass_both_2 <- intersect(ppPassAll, ppPassR2_avg)
+        if (identical(input$StrinRadio, c("1", "2"))) {
+          crit_pep <- pass_both_2
+        } else if (identical(input$StrinRadio, c("1"))) {
+          crit_pep <- ppPass_min
+        } else if (identical(input$StrinRadio, c("2"))) {
+          crit_pep <- ppPassR2_avg
+        } else {crit_pep <- ppPassAll}
         
-        grouped_fit %>% filter(Peptide %in% ppPassR2_avg) %>% 
+        grouped_fit %>% filter(Peptide %in% crit_pep) %>% 
           group_by(Peptide) %>% 
-          mutate(FC = SignalAvg[Group == "Control"]/SignalAvg[Group == "Case"]) %>%
-          filter(FC <= input$FCslider[1] | FC >= input$FCslider[2]) %>% pull(Peptide) %>% unique() -> sig_pep
+          mutate(FC = SignalAvg[Group == "Control"]/SignalAvg[Group == "Case"]) -> grouped_fit_res
+          
+          grouped_fit_res %>% filter(FC <= input$FCslider[1] | FC >= input$FCslider[2]) %>% 
+          pull(Peptide) %>% unique() -> sig_pep
         
-      
         shared_values$sig_pep <- sig_pep
         
-       output$qc_text <-renderText(paste0("removed", as.character(141 - length(ppPassAll)), " and ", as.character(141 - length(ppPassR2_avg))))
-      
+       output$qc_text <- renderText({
+         paste0("You have selected ", length(crit_pep), " substrates based on inclusion and exclusion criteria.")
+       })
+       
+       output$remain2 <- renderText({
+         paste0( as.character(length(sig_pep)), " substrates were chosen for KRSA analysis.")
+                })
        
        updateSelectInput(session, "pep_sel", choices = sig_pep)
          
@@ -346,26 +327,65 @@ server <- function(input, output, session) {
          filtered_samples %>% dplyr::filter(Peptide %in% input$pep_sel, Cycle == 124) %>% 
            mutate(Group = ifelse(`Sample name` %in% input$controlSamples, "Control", "Case")) %>% 
            ggplot(aes(`Exposure time`, Signal, group = `Sample name`)) + 
-           geom_smooth(method = lm, formula = y~x, se=F, aes(color = factor(Group)))
+           geom_smooth(method = lm, formula = y~x, se=F, aes(color = factor(Group))) -> pp1
+         
+         plots$pep <- pp1
+         
+         output$download_pep_plot <- downloadHandler(
+           file = function() { paste0(input$pep_sel, ".png") },
+           content = function(file) {
+             ggsave(file,plot=plots$pep)
+           })
+         
+         pp1
+         
        })
-
 
           output$fit_table <- DT::renderDataTable({
             head(Dataset_PW_C1_Fit)
             #}, caption = "Preview of raw data file")
           }, options=list(scrollX=TRUE, pageLength = 10))
           
-          
           output$heatmap <- renderPlot({
             
-            Dataset_PW_C1_Fit %>% select(`Sample name`, Peptide, slope) %>% 
+            Dataset_PW_C1_Fit %>% filter(Peptide %in% ppPass_min) %>% 
+              select(`Sample name`, Peptide, slope) %>% 
               pivot_wider(names_from = `Sample name`, values_from = slope) %>% 
               column_to_rownames("Peptide") %>% as.matrix() %>% log2() -> mm
             
+            output$download_heatmap <- downloadHandler( #TODO: download in portrait mode 
+              filename = "heatmap.pdf",
+              content = function(file) {
+                pdf(file, height = 10, width = 6, useDingbats = T)
+                plotInputH(mm, input$slider2[1], input$slider2[2], input$low, input$mid, input$high, input$clust)
+                dev.off()
+              })
+            
             plotInputH(mm, input$slider2[1], input$slider2[2], input$low, input$mid, input$high, input$clust)
+            
           })
           
-        
+          grouped_fit %>% filter(Peptide %in% ppPassR2_avg) %>% 
+            group_by(Peptide) %>% 
+            mutate(FC = SignalAvg[Group == "Control"]/SignalAvg[Group == "Case"]) -> grouped_fit_res
+          
+          grouped_fit_res %>% select(Peptide, FC, Group, SignalAvg) %>% 
+            pivot_wider(names_from = Group, values_from = SignalAvg) -> results_table_download1
+          
+          output$downloadDataFC <- downloadHandler(
+            filename = "substrate_results.csv",
+            content = function(file) {
+              write.csv(Dataset_PW_C1_Fit, file, row.names = FALSE)
+            }
+          )
+          
+          output$downloadDataFC2 <- downloadHandler(
+            filename = "substrate_results_2.csv",
+            content = function(file) {
+              write.csv(results_table_download1, file, row.names = FALSE)
+            }
+          )
+     
       })
       
       output$subnum2 <- renderUI({
@@ -374,12 +394,14 @@ server <- function(input, output, session) {
         HTML(paste(str1, shared_values$sig_pep, sep = '<br/>'))
       })
       
+      output$subnum3 <- renderText({
+        paste0("You have selected ", length(shared_values$sig_pep) , " significant substrates from your data set (listed below):")
+      })
       
-    
-    
-    
-    #Output for rendering the preview table in the main panel.
-    #output$contents0 <- DT::renderDataTable({
+      output$subnum4 <- renderText({
+        paste0("You have selected ", input$itr_num , " iterations.")
+      })
+
     output$contents0 <- DT::renderDataTable({
       data_set0() %>% filter(Barcode %in% input$celltype) %>% head()
         #}, caption = "Preview of raw data file")
@@ -416,10 +438,19 @@ server <- function(input, output, session) {
       output$overallTable <- DT::renderDataTable({
         krsa_output$krsa_table
       }, options=list(scrollX=TRUE, pageLength = 10))
+      
+      output$download_krsa_table <- downloadHandler(
+        filename = "KRSA_results.csv",
+        content = function(file) {
+          write.csv(krsa_output$krsa_table, file, row.names = FALSE)
+        }
+      )
      
       })
       
       updateSelectInput(session, "kin_sel", choices = krsa_output$krsa_table$Kinase)
+      
+
       
       output$kin_plot <- renderPlot({
         krsa_output$res %>% rename(Kinase = Kin) %>% 
@@ -430,23 +461,41 @@ server <- function(input, output, session) {
                     fill="gray", alpha=0.5
           ) +
           geom_vline(data=filter(krsa_output$krsa_table, Kinase == input$kin_sel), aes(xintercept = Observed), color = "red", size = 1,show.legend = F) +
-          labs(x = "Hits", y = "Counts", title = input$kin_sel)
+          labs(x = "Hits", y = "Counts", title = input$kin_sel) + 
+          theme(plot.title = element_text(hjust = 0.5)) -> pp2
+
+        
+        plots$hist <- pp2
+        
+        output$download_hist <- downloadHandler(
+          file = function() { paste0(input$kin_sel, ".png") },
+          content = function(file) {
+            ggsave(file,plot=plots$hist)
+            #ggsave(filename=file, plot = plotInputG(), device = "pdf")
+          })
+        
+        pp2
+
       })
       
       output$networkPlot <- renderPlot({
         filter(krsa_output$krsa_table, abs(Z) >= 2) %>% pull(Kinase) -> kin_hits
+        
+        output$download_net <- downloadHandler(
+          filename = "network.pdf",
+          content = function(file) {
+            pdf(file)
+            plotInputN(kin_hits, input$hitsColor, input$interColor, input$nodeSize, input$nodeTextSize, input$layout)
+            dev.off()
+          })
+        
         plotInputN(kin_hits, input$hitsColor, input$interColor, input$nodeSize, input$nodeTextSize, input$layout)
+        
       })
-      
-      
-      
+    
       
     })
-    
-    
-    
-    
-    
+  
 }
 
 
